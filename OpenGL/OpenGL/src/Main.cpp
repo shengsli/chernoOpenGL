eaec74a4df7545035b4e46f85e6f86a78fe5553a
shengsli <sheng.s.li@outlook.com>
2019-09-26 09:40:55 +0800

    error code, to be fixed

--- OpenGL/OpenGL/src/Main.cpp
+++ OpenGL/OpenGL/src/Main.cpp
@@ -90,11 +90,20 @@ static int CreateShader(const std::string& vertexShader, const std::string& frag
 int main(void) {
 	GLFWwindow* window;
 	if (!glfwInit()) return -1;
+
+	//vertex array objects are mandatory, they are in use even without us creating them. 
+	//opengl compatability profile is a mode in opengl running by default. 
+	//core profile requires us bind vertex array objects manually. 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);/* Make the window's context current */
 	glfwSwapInterval(1);
 	if (glewInit() != GLEW_OK) std::cout << "Error!" << std::endl;
@@ -112,11 +121,15 @@ int main(void) {
 		2,3,0
 	};
 
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+
 	// we still need this vertex buffer
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));//generate buffer
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));//bind buffer before draw
-	GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));//put data into buffer
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));//put data into buffer
 
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void *)0));
@@ -125,24 +138,39 @@ int main(void) {
 	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));//generate buffer
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));//bind buffer before draw
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(unsigned int), indices, GL_STATIC_DRAW));//put data into buffer, has to be unsigned
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));//put data (i.e. 6 indices) into buffer, has to be unsigned
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-	//std::cout << source.VertexSource << std::endl;
-	//std::cout << source.FragmentSource << std::endl;
-
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource); 
 	GLCall(glUseProgram(shader));//bound shader
-	int location = glGetUniformLocation(shader, "u_Color");//retrieve location of u_Color
+
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));//retrieve location of u_Color
 	ASSERT(location != -1);//-1 if the uniform is not found or is unused. 
 	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));//4 floats, int location
+
+	//unbind everything
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));//unbind shader
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));//unbind vertex buffer
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));//unbind index buffer
 
 	float r = 0.0f;
 	float increment = 0.05f;
 	while (!glfwWindowShouldClose(window)) {/* Loop until the user closes the window */
 		glClear(GL_COLOR_BUFFER_BIT);/* Render here */
 
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));//4 floats, int location
+
+		//vertex buffer
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+		GLCall(glEnableVertexAttribArray(0));//set up the layout. do this every time, cuz it might change if we draw another object with a different layout. 
+		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void *)0));//do this every time
+		
+		//jsut bind vertex array and index buffer
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		if (r > 1.0f)
